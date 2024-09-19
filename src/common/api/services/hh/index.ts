50c9b68cc6a2100b2d1d6cb2e0ce3f0cbf99516d
Andrey <kuropatkinn.a.i21@gmail.com>
2024-09-19 10:16:38 +0300

    fix: limit resumes

--- src/common/api/services/hh/index.ts
+++ src/common/api/services/hh/index.ts
@@ -4,7 +4,7 @@ export const HhruService = {
   async getResumes(params: any) {
     const response = await axiosHhruInstance.get(`/resumes?${params}`, {
       params: {
-        per_page: 1,
+        per_page: 5,
       },
     });
     return response.data;
